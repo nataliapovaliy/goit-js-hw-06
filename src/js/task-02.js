7f655990928fcfbd5f2a43b8e48cbd19e4ff38d4
nataliapovaliy <natalia.bg1125@gmail.com>
2022-11-29 18:49:32 +0200

    task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+
+for (const i of ingredients) {
+  let lastEl = document.createElement("li");
+  lastEl.textContent = i;
+  listIngredients.append(lastEl);
+}
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,19 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const listGallery = document.querySelector('.gallery');
+
+// for (const i of images) {
+//   let lastEl = document.createElement("li");
+//   lastEl.textContent = i;
+//   listIngredients.append(lastEl);
+// }
+
+
+
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+//* <ul class="gallery"></ul>
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи. */}
